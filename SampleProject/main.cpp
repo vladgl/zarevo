01a59fed57d7aab4aa7ee635aac68c88cced8cf6
metoole <metoolenz@gmail.com>
2020-10-28 18:50:03 +0300

    Added memory leak check to SampleProject

--- SampleProject/main.cpp
+++ SampleProject/main.cpp
@@ -4,12 +4,24 @@
 #include <zarevo.h>
 #include "mWindow.h"
 
+#ifdef ZRV_DEBUG
+    #define __CRTDBG_MAP_ALLOC
+    #include <crtdbg.h>
+    #define DEBUG_NEW new(_NORMAL_BLOCK, __FILE__, __LINE__)
+    #define new DEBUG_NEW
+#endif
+
 using namespace std;
 
 int main()
 {
 	ZRV_LOG << "Proj started\n";
 	mWindow wnd(1000, 800, "Sample Project");
-	return wnd.exec();
+    auto return_val = wnd.exec();
+	
+    #ifdef __CRTDBG_MAP_ALLOC
+        _CrtDumpMemoryLeaks();
+    #endif
 
+    return return_val;    
 }
